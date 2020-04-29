0de885e890c672de2975ae5cf0ce87bbbc82d62e
Eduardo DdC <eduardo-dcs@outlook.es>
2020-04-28 21:44:39 -0500

    Update talker.py
    Arreglar rate

--- my_first_package/scripts/talker.py
+++ my_first_package/scripts/talker.py
@@ -9,7 +9,7 @@ from std_msgs.msg import Int32
 def talker():
 	pub = rospy.Publisher('chatter', Int32, queue_size=10)
 	rospy.init_node('talker', anonymous=True)
-	rate = rospy.Rate(10) #10hz
+	rate = rospy.Rate(0.2) #0.2Hz=5seg
 	while not rospy.is_shutdown():
 		num_str = (random.randrange(10000)) % rospy.get_time()
 		rospy.loginfo(num_str)
